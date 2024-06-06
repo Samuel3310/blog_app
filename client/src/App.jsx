04d0496f16892da53fb596eed43072fbcf99852f
samuel3310 <awoluowolabisamuel@gmail.com>
2024-06-06 11:37:27 +0100

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Dashboard from "./pages/dashboard";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import Project from "./pages/project";
 
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
